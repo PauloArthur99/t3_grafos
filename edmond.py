8d96f329f71820702ab366180cb63296f2d5581a
Snitzelberg <48810985+Snitzelberg@users.noreply.github.com>
2021-09-14 17:31:00 -0300

    Edmond e alguns testes

---
+++ edmond.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+from biblioteca_grafo import Grafo
+
+def ford_fukerson(grafo,vert_origem,vert_destino):
+    
+    fluxo = 0
+    caminho = edmond_karp(grafo,vert_origem,vert_destino)
+    
+    while len(caminho) != 0:   
+        
+        fluxo_i = 99999
+        
+        for i in range(len(caminho)-1):
+            fluxo_i = min(grafo.peso(caminho[i],caminho[i+1]),fluxo_i)
+        
+        for i in range(len(caminho)-1):
+            grafo.pesos[(caminho[i],caminho[i+1])] -= fluxo_i
+        
+        fluxo += fluxo_i
+        caminho = edmond_karp(grafo,vert_origem,vert_destino)
+
+    return(fluxo) 
+
+def edmond_karp(grafo,vert_origem,vert_destino):
+    
+    c = [False] * (grafo.qtdVertices()+1)
+    a = [None] * (grafo.qtdVertices()+1)
+    c[vert_origem] = True
+    q = [vert_origem]
+
+    while len(q) != 0:
+        u = q.pop()
+
+        for vizinho in grafo.vizinhos(u):
+            
+            if c[vizinho] == False and grafo.peso(u,vizinho) > 0:
+                c[vizinho] == True
+                a[vizinho] = u
+                
+                if vizinho == vert_destino:
+                    p = [vert_destino]
+                    w = vert_destino
+                    
+                    while w != vert_origem:
+                        w = a[w]
+                        p.append(w)
+                    
+                    p.reverse()
+                    return p
+                q.insert(0,vizinho)#
+    return []
+
+grafo1 = Grafo("db512.gr")
+print(edmond_karp(grafo1,1,4))
+print(ford_fukerson(grafo1,1,(grafo1.qtdVertices())))
